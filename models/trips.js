6e0b46f1221054eb56b8f8340db96aba7162b6dc
Elison Liu <elisonliu97@gmail.com>
2021-06-24 10:36:11 -0400

    created database and models

---
+++ models/trips.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Trip extends Model {}
+
+Trip.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        trip_budget: {
+            type: DataTypes.INTEGER,
+        },
+        traveller_amount: {
+            type: DataTypes.INTEGER,
+        },
+        traveller_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'traveller',
+                key: 'id',
+            }
+        },
+        location_id: {
+            type: DataTypes.INTEGER,
+            references: { 
+                model: 'location',
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'trip'
+    }
+);
+
+module.exports = Trip;
