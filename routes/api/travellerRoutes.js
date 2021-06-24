8e96901667c0063849ea309decdfd8dd90311f87
Elison Liu <elisonliu97@gmail.com>
2021-06-24 11:44:41 -0400

    created routing for traveller data

--- routes/api/travellerRoutes.js
+++ routes/api/travellerRoutes.js
@@ -13,17 +13,44 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        const travellerData = await Traveller.findByPk(req.params.id ,{
+        const travellerData = await Traveller.findByPk(req.params.id, {
             include: [{ model: Trip }],
         })
-        if (!travellerData){
-            res.status(404).json({ message: 'No traveller found with this id'});
+        if (!travellerData) {
+            res.status(404).json({ message: 'No traveller found with this id' });
             return;
         } else {
             res.status(200).json(travellerData);
         }
     } catch (err) {
         res.status(500).json(err);
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const travellerData = await Traveller.create(req.body);
+        res.status(200).json(travellerData);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const travellerData = await Traveller.destroy({
+            where: {
+                id: req.params.id,
+            }
+        });
+        if (!travellerData){
+            res.status(404).json({ message: 'No traveller found with this id' });
+            return;
+        } else {
+            res.status(200).json({ message: "Traveller deleted" })
+        }
+    } catch (err) {
+        res.status(500).json(err)
     }
 })
 
